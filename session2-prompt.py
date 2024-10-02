eef70a928c78131e918ebd66f798210e7ccfddda
Alexander Solorzano <114269725+Humberto233@users.noreply.github.com>
2024-10-01 20:53:21 -0700

    Add files via upload

---
+++ session2-prompt.py
@@ -0,0 +1,16 @@
+'''Write a Python that prints the sum of two floating point numbers, 
+the difference between two integers, and the product of a floating point number and an integer.'''
+import numpy as np
+
+a = 2.67
+b = 4.35
+c = 3
+d = 12
+
+sum = a + b
+difference = c - d
+product = a * c
+
+print("Here is the sum:", sum)
+print("Here is the difference:", difference)
+print("Here is the product:", product)
